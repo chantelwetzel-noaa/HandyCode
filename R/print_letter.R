8ed09658578d045a93a24fc00725259f5f27186c
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-07-14 07:35:08 -0700

    Initial Package Creation

---
+++ R/print_letter.R
@@ -0,0 +1,10 @@
+#' Add text to figure
+#' @param label text
+#' @param xy location on figure to add text
+#' @authorwritten by Chantel Wetzel
+print.letter <- function(label="(a)",xy=c(0.1,0.925),...) {   #... means any new parameters are copied faithfully to text() below
+  tmp <- par("usr")
+  text.x <- tmp[1]+xy[1]*diff(tmp[1:2])   #x position, diff=difference
+  text.y <- tmp[3]+xy[2]*diff(tmp[3:4])   #y position
+  text(x=text.x, y=text.y, labels=label, ...)             
+}
